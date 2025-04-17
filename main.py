933fd17f65feef3c38967e45b26812a1cd414926
trekitch <trekitch@ymail.com>
2025-04-17 09:38:28 -0400

    removed startup logging

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         screen.fill("black")
