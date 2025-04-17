58733499320f67004b294d3f6ac1fdfc9df7a1d6
trekitch <trekitch@ymail.com>
2025-04-17 10:20:21 -0400

    added player rendering and rotation

--- main.py
+++ main.py
@@ -3,17 +3,26 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
-        screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 
 
