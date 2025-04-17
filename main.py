e495e973eebfc7773bbc47c09b30c19ebbdb3168
trekitch <trekitch@ymail.com>
2025-04-17 09:37:36 -0400

    Initial commit for asteroids game.

---
+++ main.py
@@ -0,0 +1,26 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
